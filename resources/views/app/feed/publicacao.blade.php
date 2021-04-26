790ee10ed8f00e56ecd2c497ffc465f8a4114732
MarcosDanielVieira <30511764+MarcosDanielVieira@users.noreply.github.com>
2021-04-26 12:39:22 -0300

    Organizando códigos

--- resources/views/app/feed/publicacao.blade.php
+++ resources/views/app/feed/publicacao.blade.php
@@ -110,39 +110,39 @@
           
           {{-- @include('app.feed.feeds') --}}
           
-            <h2>
-              Feeds
-            </h2>
+          <h2>
+            Feeds
+          </h2>
 
-            @isset($objPublicacao)
-              @foreach ($objPublicacao as $item)
-              <div class="div-publicacao-feed">
+          @isset($objPublicacao)
+            @foreach ($objPublicacao as $item)
+            <div class="div-publicacao-feed">
 
-                <p class="nome-perfil-publicacao sombraDiv">
-                  {{$item->Usuario}}
-                </p>
-                
-                <p>
-                  <small>
-                    Publicado no dia {{$item->Publicado}}
-                  </small>
-                </p>
+              <p class="nome-perfil-publicacao sombraDiv">
+                {{$item->Usuario}}
+              </p>
+              
+              <p>
+                <small>
+                  Publicado no dia {{$item->Publicado}}
+                </small>
+              </p>
 
-                <p class="texto-publicacao">
-                  {{$item->Publicacao}}
-                </p>
+              <p class="texto-publicacao">
+                {{$item->Publicacao}}
+              </p>
 
-                {{--
-                  Renderizando comentário se existir
-                --}}
+              {{--
+                Renderizando comentário se existir
+              --}}
 
-                @include('App.Feed.comentario')
+              @include('App.Feed.comentario')
 
-              </div>
-              @endforeach
-            @endisset
+            </div>
+            @endforeach
+          @endisset
 
-            <div class="feeds"></div>
+          <div class="feeds"></div>
 
         </div>
       </div>
