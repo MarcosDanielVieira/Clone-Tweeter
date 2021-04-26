a6439068c56012c9fce83024d7b9a7f427c6588b
MarcosDanielVieira <30511764+MarcosDanielVieira@users.noreply.github.com>
2021-04-26 12:41:12 -0300

    renderizando view

--- resources/views/app/feed/publicacao.blade.php
+++ resources/views/app/feed/publicacao.blade.php
@@ -154,6 +154,39 @@
     Renderizando tweetrs menos do usario logado
   --}}
   
-  @include('App.Feed.tweetrs')
+  {{-- @include('App.Feed.tweetrs') --}}
+
+  
+  <div class="col-3 divSeguir">
+    <h3>Tweeters</h3>
+    <hr/>
+    
+    {{-- 
+      Listando todos os usuarios
+    --}}
+
+      @isset($objUsuario)
+        
+        @foreach ($objUsuario as $usuario)
+
+          {{-- 
+            Verificando se não é usuario logado
+          --}}
+
+          @if ($usuario->id != session("guarda.usuarioLogado"))
+            
+            {{-- 
+              Renderizando view seguir
+            --}}
+          
+            @include('App.Form.Seguir')
+              
+          @endif
+
+        @endforeach
+
+      @endisset
+  
+  </div>
   
 </div>
