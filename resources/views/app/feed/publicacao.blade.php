ebf91fc5a0addc238210b08bd33636d88c80c082
MarcosDanielVieira <30511764+MarcosDanielVieira@users.noreply.github.com>
2021-04-26 12:36:48 -0300

    renderizando feed

--- resources/views/app/feed/publicacao.blade.php
+++ resources/views/app/feed/publicacao.blade.php
@@ -108,7 +108,43 @@
             Renderizando resultado da transição de página
           --}}
           
-          @include('App.feed.feeds')
+          {{-- @include('App.feed.feeds') --}}
+
+          
+            <h2>
+              Feeds
+            </h2>
+
+            @isset($objPublicacao)
+              @foreach ($objPublicacao as $item)
+              <div class="div-publicacao-feed">
+
+                <p class="nome-perfil-publicacao sombraDiv">
+                  {{$item->Usuario}}
+                </p>
+                
+                <p>
+                  <small>
+                    Publicado no dia {{$item->Publicado}}
+                  </small>
+                </p>
+
+                <p class="texto-publicacao">
+                  {{$item->Publicacao}}
+                </p>
+
+                {{--
+                  Renderizando comentário se existir
+                --}}
+
+                @include('App.Feed.comentario')
+
+              </div>
+              @endforeach
+            @endisset
+
+            <div class="feeds"></div>
+
           
         </div>
       </div>
